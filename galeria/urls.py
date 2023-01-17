a22c87f0250950f6b2d9cf97715b3aa29bc168a9
Yuri Timafejn <yuri.timafejn@ecgroup.com>
2023-01-17 16:07:38 -0300

    Correção de erros de digitação e criação dos templates producao e index

--- galeria/urls.py
+++ galeria/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from galeria.views import index, registro
 
 urlpatterns = [
-    path('', index),
-    path('produção/', registro)
+    path('', index, name='listagem_producoes'),
+    path('producao/', registro, name='producao'),
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -4,4 +4,4 @@ def index(request):
     return render(request, 'galeria/index.html')
 
 def registro(request):
-    return render(request, 'galeria/filme_serie.html')
+    return render(request, 'galeria/producao.html')
